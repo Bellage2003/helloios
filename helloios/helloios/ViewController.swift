6fd552fff77e21eed969fc56a9d2b686b3e9ae3a
Bellage <gehuijun@uw.edu>
2024-03-28 21:01:32 -0700

    Finished and added the screenshots

---
+++ helloios/helloios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  helloios
+//
+//  Created by BellaGe on 2024/3/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
